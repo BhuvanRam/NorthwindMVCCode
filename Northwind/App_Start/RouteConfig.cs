74ce5214ba3f601efe26c48021729c29a228dc38
BhuvanPerakam <bhuvan.perakam@gmail.com>
2016-09-04 16:24:23 +0530

    Changes

--- Northwind/App_Start/RouteConfig.cs
+++ Northwind/App_Start/RouteConfig.cs
@@ -11,6 +11,7 @@ namespace Northwind
     {
         // Verifying the RouteConfig
 
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
